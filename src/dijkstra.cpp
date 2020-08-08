a04381e4c5d1b962a3a0531f65804689ff66cbcc
Daniel Kinsman <danielkinsman@riseup.net>
2020-08-08 18:47:29 +1000

    stubbed api

--- src/dijkstra.cpp
+++ src/dijkstra.cpp
@@ -4,7 +4,14 @@ using namespace std;
 using namespace godot;
 
 void Dijkstra::_register_methods() {
-    register_method("dummy", &Dijkstra::dummy);
+    register_method("add_node", &Dijkstra::add_node);
+    register_method("remove_node", &Dijkstra::remove_node);
+    register_method("get_position", &Dijkstra::get_position);
+    register_method("add_edge", &Dijkstra::add_edge);
+    register_method("remove_edge", &Dijkstra::remove_edge);
+    register_method("solve", &Dijkstra::solve);
+    register_method("get_next_node_towards_source", &Dijkstra::get_next_node_towards_source);
+    register_method("get_next_node_position_towards_source", &Dijkstra::get_next_node_position_towards_source);
 }
 
 Dijkstra::Dijkstra() {
@@ -16,6 +23,29 @@ Dijkstra::~Dijkstra() {
 void Dijkstra::_init() {
 }
 
-Variant Dijkstra::dummy() {
-    return String("hello");
+void Dijkstra::add_node(int id, Vector2 position) {
+}
+
+void Dijkstra::remove_node(int id) {
+}
+
+Vector2 Dijkstra::get_position(int id) {
+    return Vector2();
+}
+
+void Dijkstra::add_edge(int from, int to, int weight) {
+}
+
+void Dijkstra::remove_edge(int from, int to) {
+}
+
+void Dijkstra::solve(int source) {
+}
+
+int Dijkstra::get_next_node_towards_source(int id) {
+    return 0;
+}
+
+Vector2 Dijkstra::get_next_node_position_towards_source(Vector2 position) {
+    return Vector2();
 }
--- src/dijkstra.h
+++ src/dijkstra.h
@@ -3,6 +3,7 @@
 
 #include <Godot.hpp>
 #include <Object.hpp>
+#include <Vector2.hpp>
 
 using namespace std;
 
@@ -18,7 +19,14 @@ public:
     ~Dijkstra();
 
     void _init(); // our initializer called by Godot
-    Variant dummy();
+    void add_node(int id, Vector2 position=Vector2());
+    void remove_node(int id);
+    Vector2 get_position(int id);
+    void add_edge(int from, int to, int weight=1);
+    void remove_edge(int from, int to);
+    void solve(int source);
+    int get_next_node_towards_source(int id);
+    Vector2 get_next_node_position_towards_source(Vector2 position);
 };
 
 }
